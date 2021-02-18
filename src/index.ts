e94e682ff2ceb468bf23ea6f16285531f4dbbcfb
Adonys <adonyssantoscemas@gmail.com>
2021-02-18 13:26:09 -0400

    Limit y Paginación

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,45 @@ import { returnDocuments } from './helpers/show-documents';
 
 const usersRef = db.collection('users')
 
-usersRef
-	.orderBy('name')
-	.orderBy('salary')
-	.get().then( returnDocuments )
+//Btn Next
+const btnNext = document.createElement('button');
+btnNext.innerText = "Next Page";
+document.body.append( btnNext );
+
+let firstDocument: any = null;
+let lastDocument: any = null;
+btnNext.addEventListener('click', () => {
+
+	const querry = usersRef
+		.orderBy('name')
+		.startAfter( lastDocument )
+
+	querry.limit(2).get().then( snap => {
+
+		firstDocument = snap.docs[ 0 ] || null;
+		lastDocument = snap.docs[ snap.docs.length - 1] || null;
+		returnDocuments(snap);
+	})
+
+})
+
+btnNext.click();
+
+//Btn Previous
+const btnPrevious = document.createElement('button');
+btnPrevious.innerText = "Previous Page";
+document.body.append( btnPrevious );
+
+btnPrevious.addEventListener('click', () => {
+
+	const querry = usersRef
+		.orderBy('name')
+		.endBefore( firstDocument )
+
+	querry.limit(2).get().then( snap => {
+
+		firstDocument = snap.docs[ 0 ] || null;
+		lastDocument = snap.docs[ snap.docs.length - 1] || null;
+		returnDocuments(snap);
+	})
+})
