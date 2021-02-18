7a617a0eda580e05cfce1b701c05d37b651dabe4
Adonys <adonyssantoscemas@gmail.com>
2021-02-18 12:31:12 -0400

    orderBy

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,10 @@
-import db from './firestore/config'
-import { returnDocuments } from './helpers/show-documents'
-
-const user = {
-	name: "Evelyn",
-	active: true,
-	birthday: 0
-}
+import db from './firestore/config';
+import { returnDocuments } from './helpers/show-documents';
+// import { firestore } from 'firebase';
 
 const usersRef = db.collection('users')
 
-// db.collection('users')
-// 	.add( user )
-// 	.then( ref => {
-// 		console.log(ref.delete());
-// 	} )
-// 	.catch( e => console.log("error", e))
-
-// usersRef
-// 	.doc('wx7x0X4HWkNgflaQKWLP')
-// 	.update({
-// 		active: false
-// 	})
-
-// usersRef
-// 	.doc('wx7x0X4HWkNgflaQKWLP')
-// 	.set({
-// 		active: false,
-// 		age: 17
-// 	})
-
-// usersRef
-// 	.doc('wx7x0X4HWkNgflaQKWLP')
-// 	.delete()
-// 	.then( () => console.log("Deleted!") )
-// 	.catch( e => console.log('error', e) )
-
-// usersRef
-// 	.onSnapshot( returnDocuments )
-
-// usersRef.get().then( returnDocuments )
-
-// usersRef.where('active', '==', true).get().then( returnDocuments )
-
-// usersRef.where('salary', '>', 1800)
-	// .get().then( returnDocuments )
-
-// usersRef.where('salary', '>=', 1800)
-// 		.where('salary', '<=', 2300)
-// 	.get().then( returnDocuments )
-
-usersRef.where('salary', '>=', 1800)
-		.where('active', '==', true)
+usersRef
+	.orderBy('name')
+	.orderBy('salary')
 	.get().then( returnDocuments )
