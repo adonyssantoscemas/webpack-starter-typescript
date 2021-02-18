4c878ab74a22ed07ffcf2f00d6c1f11c85963dba
Adonys <adonyssantoscemas@gmail.com>
2021-02-18 11:09:43 -0400

    Insertar data en Firestore

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import './firestore/config'
+import db from './firestore/config'
 
+const user = {
+	name: "Evelyn",
+	active: true,
+	birthday: 0
+}
+
+// db.collection('users')
+// 	.add( user )
+// 	.then( ref => {
+// 		console.log(ref.delete());
+// 	} )
+// 	.catch( e => console.log("error", e))
 
